babb1dd8406a44a336fdee0af078bc1c0d5f6709
Stormacq, Sebastien <stormacq@amazon.lu>
2019-12-01 19:26:39 -0500

    asynchronous loading for images

--- MOB304/code/Complete/Landmarks/Landmarks/Models/Landmark.swift
+++ MOB304/code/Complete/Landmarks/Landmarks/Models/Landmark.swift
@@ -8,8 +8,29 @@ The model for an individual landmark.
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable, Identifiable {
-    var id: String
+// migrated Landmark from struct to class to make it Observable
+class Landmark: Decodable, Identifiable, ObservableObject {
+    
+    // consequence is that I need to add the constructor
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: LandmarkKeys.self) // defining our (keyed) container
+        id          = try container.decode(Int.self, forKey: .id)
+        name        = try container.decode(String.self, forKey: .name)
+        imageName   = try container.decode(String.self, forKey: .imageName)
+        state       = try container.decode(String.self, forKey: .state)
+        park        = try container.decode(String.self, forKey: .park)
+        isFavorite  = try container.decode(Bool.self, forKey: .isFavorite)
+        category    = try container.decode(Category.self, forKey: .category)
+        coordinates = try container.decode(Coordinates.self, forKey: .coordinates)
+        
+        // set a place holder image
+        image = ImageStore.shared.placeholder()
+        
+        // trigger image download
+        let _ = ImageStore.shared.image(name: imageName, landmark: self)
+    }
+    
+    var id: Int
     var name: String
     fileprivate var imageName: String
     fileprivate var coordinates: Coordinates
@@ -30,11 +51,20 @@ struct Landmark: Hashable, Codable, Identifiable {
         case rivers = "Rivers"
         case mountains = "Mountains"
     }
-}
-
-extension Landmark {
-    var image: Image {
-        ImageStore.shared.image(name: imageName)
+    
+    // advertise changes on this property.  This will allow Views to refresh when image is changed.
+    @Published var image : Image
+    
+    // part of Decodable protocol, I need to declare all keys from the jSON file
+    enum LandmarkKeys: String, CodingKey {
+        case id          = "id"
+        case name        = "name"
+        case imageName   = "imageName"
+        case category    = "category"
+        case isFavorite  = "isFavorite"
+        case park        = "park"
+        case state       = "state"
+        case coordinates = "coordinates"
     }
 }
 
